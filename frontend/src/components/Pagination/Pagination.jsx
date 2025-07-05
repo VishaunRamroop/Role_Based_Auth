9792f471ab5bf75d72622da80e53573d2e02633a
VshaunR <vramshaun@gmail.com>
2025-07-05 12:39:18 -0400

    pixed pagination bug, added meta tags

--- frontend/src/components/Pagination/Pagination.jsx
+++ frontend/src/components/Pagination/Pagination.jsx
@@ -25,13 +25,13 @@ const {totalPages,page,setPage} = useProductContext();
 
   return (
     <div className='flex justify-center items-center gap-2 bg-gray-200 p-2 rounded-md '>
-      <CustomButton className={`bg-sky-400 rounded font-bold`} onClick={()=>setPage(p=>p-1)}>Prev</CustomButton>
+      <CustomButton className={`bg-sky-400 rounded font-bold`} onClick={()=>setPage(p=>p<=1?1:p-1)}>Prev</CustomButton>
       {totalP.map((p,index)=>{
       return  <CustomButton className={`` + (p===page ? 'bg-blue-500 text-white p-2 rounded-lg' : 'bg-gray-300 text-black')} key={index} onClick={()=>{if(p!=='...')setPage(p)}}>
         {p}
         </CustomButton>
       })}
-      <CustomButton className={`bg-sky-400 rounded font-bold`} onClick={()=>setPage(p=>p+1)}>Next</CustomButton>
+      <CustomButton className={`bg-sky-400 rounded font-bold`} onClick={()=>setPage(p=>p===totalPages?totalPages:p+1)}>Next</CustomButton>
     </div>
   )
 }
